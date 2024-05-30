8d9eccaf2f9df342b4dbe4457a832ad35a804855
Ragazzi147 <91346278+Ragazzi147@users.noreply.github.com>
2024-05-30 17:06:08 -0300

    EmailScreen

--- screens/EmailScreen.js
+++ screens/EmailScreen.js
@@ -1,11 +1,93 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import React, {useState} from 'react';
+import Fontisto from 'react-native-vector-icons/Fontisto';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import {useNavigation} from '@react-navigation/native';
 
 const EmailScreen = () => {
+  const [email, setEmail] = useState('');
+  const navigation = useNavigation();
+  const handleNext = () => {
+    navigation.navigate('Password');
+  };
+
   return (
-    <View>
-      <Text>EmailScreen</Text>
-    </View>
+    <SafeAreaView style={{flex: 1, backgroundColor: 'white'}}>
+      <View style={{marginTop: 90, marginHorizontal: 20}}>
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+          <View
+            style={{
+              width: 44,
+              height: 44,
+              borderRadius: 22,
+              borderColor: 'black',
+              borderWidth: 2,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Fontisto name="email" size={26} color="black" />
+          </View>
+          <Image
+            style={{width: 100, height: 40}}
+            source={{
+              uri: 'https://cdn-icons-png.flaticon.com/128/10613/10613685.png',
+            }}
+          />
+        </View>
+        <Text
+          style={{
+            fontSize: 25,
+            fontWeight: 'bold',
+            fontFamily: 'GeezaPro-Bold',
+            marginTop: 15,
+          }}>
+          Insira um Email valido
+        </Text>
+        <Text style={{marginTop: 30, fontSize: 15, color: 'gray'}}>
+          Verificação de email deixa mantem sua conta segura.{' '}
+          <Text style={{color: '#581845', fontWeight: '500'}}>Saiba mais</Text>
+        </Text>
+        <TextInput
+          value={email}
+          onChangeText={text => setEmail(text)}
+          placeholder="Enter your email"
+          placeholderTextColor={'#BEBEBE'}
+          style={{
+            width: 340,
+            marginVertical: 10,
+            fontSize: 22,
+            marginTop: 25,
+            borderBottomColor: 'black',
+            borderBottomWidth: 1,
+            paddingBottom: 10,
+            fontFamily: 'GeezaPro-Bold',
+          }}
+        />
+
+        <Text style={{color: 'gray', fontSize: 15, marginTop: 7}}>
+          Nota: Você será solicitado a verificar seu e-mail.
+        </Text>
+        <TouchableOpacity
+          onPress={handleNext}
+          activeOpacity={0.8}
+          style={{marginTop: 30, marginLeft: 'auto'}}>
+          <MaterialCommunityIcons
+            name="arrow-right-circle"
+            size={45}
+            color="#581845"
+            style={{alignSelf: 'center', marginTop: 20}}
+          />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 };
 
