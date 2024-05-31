8b5019eeb4adcc1adc03c5dd0ade9cdd10b4e794
unknown <alm1427@hotmail.com>
2024-05-31 08:10:35 -0300

    preFinal

--- screens/PreFinalScreen.js
+++ screens/PreFinalScreen.js
@@ -1,11 +1,62 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, SafeAreaView, Pressable} from 'react-native';
 import React from 'react';
+import LottieView from 'lottie-react-native';
+import {useNavigation} from '@react-navigation/native';
 
 const PreFinalScreen = () => {
   return (
-    <View>
-      <Text>PreFinalScreen</Text>
-    </View>
+    <SafeAreaView style={{flex: 1, backgroundColor: 'white'}}>
+      <View style={{marginTop: 80}}>
+        <Text
+          style={{
+            fontSize: 35,
+            fontWeight: 'bold',
+            fontFamily: 'GeezaPro-Bold',
+            marginLeft: 20,
+          }}>
+          All set to register
+        </Text>
+        <Text
+          style={{
+            fontSize: 33,
+            fontWeight: 'bold',
+            fontFamily: 'GeezaPro-Bold',
+            marginLeft: 20,
+            marginTop: 10,
+          }}>
+          Setting up your profile for you
+        </Text>
+      </View>
+
+      <View>
+        <LottieView
+          style={{
+            height: 260,
+            width: 300,
+            alignSelf: 'center',
+            marginTop: 40,
+            justifyContent: 'center',
+          }}
+          source={require('../assets/love.json')}
+          autoPlay
+          loop={true}
+          speed={0.7}
+        />
+      </View>
+
+      <Pressable
+        style={{backgroundColor: '#900C3F', padding: 15, marginTop: 'auto'}}>
+        <Text
+          style={{
+            textAlign: 'center',
+            color: 'white',
+            fontWeight: '600',
+            fontSize: 15,
+          }}>
+          Finish registering
+        </Text>
+      </Pressable>
+    </SafeAreaView>
   );
 };
 
