508bde5ad24bfe002f1bc755fa527e78b2af9626
unknown <alm1427@hotmail.com>
2024-05-31 05:20:42 -0300

    PhotoScreen2

--- screens/PromptsScreen.js
+++ screens/PromptsScreen.js
@@ -4,7 +4,38 @@ import React from 'react';
 const PromptsScreen = () => {
   return (
     <View>
-      <Text>PromptsScreen</Text>
+      <View style={{marginTop: 90, marginHorizontal: 20}}>
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+          <View
+            style={{
+              width: 44,
+              height: 44,
+              borderRadius: 22,
+              borderColor: 'black',
+              borderWidth: 2,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <AntDesign name="photo-camera-back" size={22} color="black" />
+          </View>
+
+          <Image
+            style={{width: 100, height: 40}}
+            source={{
+              uri: 'https://cdn-icons-png.flaticon.com/128/10613/10613685.png',
+            }}
+          />
+        </View>
+        <Text
+          style={{
+            fontSize: 25,
+            fontWeight: 'bold',
+            fontFamily: 'GezzaPro-Bold',
+            marginTop: 15,
+          }}>
+          Escreva as respostas do seu perfil
+        </Text>
+      </View>
     </View>
   );
 };
