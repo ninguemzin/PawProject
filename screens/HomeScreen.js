113cb90ca23aaa075115dbf13d7f4d7ad1742a07
unknown <alm1427@hotmail.com>
2024-06-02 20:34:10 -0300

    first commit

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,6 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React {useEffect} from 'react';
-import AsyncStorage from ''
+import React, {useEffect} from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const HomeScreen = () => {
   useEffect(() => {
